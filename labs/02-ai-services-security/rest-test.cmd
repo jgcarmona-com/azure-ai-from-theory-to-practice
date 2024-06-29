f3be35d7ad8b4c3e43d0dc8b017313c1420e0e5e
Juan G. Carmona <juan@jgcarmona.com>
2024-06-29 11:31:02 +0200

    Lab 2 is ready to go -> UPDATE references and so on

---
+++ labs/02-ai-services-security/rest-test.cmd
@@ -0,0 +1,1 @@
+curl -X POST "https://azure-ai-from-theory-to-practice-lab-02.cognitiveservices.azure.com/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <KEY>" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
